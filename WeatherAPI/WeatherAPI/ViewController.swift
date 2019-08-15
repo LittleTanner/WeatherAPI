52d1b86442a6612afc922949ba1711fc286f7a51
Kevin Tanner <kevindavidtanner@gmail.com>
2019-08-15 13:46:09 -0600

    Initial commit

---
+++ WeatherAPI/WeatherAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAPI
+//
+//  Created by Kevin Tanner on 8/15/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
